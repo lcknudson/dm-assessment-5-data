5ba5395b760dfe92392e5ba5875037573358c97f
Lorin Knudson <lcknudson@gmail.com>
2023-06-15 20:38:56 -0600

    Part 2.1 complete

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,16 @@
+require("dotenv").config()
 
+const {CONNECTION_STRING} = process.env;
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres", 
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
